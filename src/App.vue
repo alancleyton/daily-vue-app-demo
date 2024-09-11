03f86ea8db2ed38bcf7b6835f7f27c7bc85a83e7
Alan Cleyton <alancleyton67@gmail.com>
2024-09-11 14:35:48 -0300

    chore: add tailwindCSS

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h2>Hello World</h2>
+    <button
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
+      Click me
+    </button>
   </div>
 </template>
 
